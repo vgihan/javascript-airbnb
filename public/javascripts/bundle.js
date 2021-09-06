36497416af60155709c88e1bad3534bce61aa8e5
J017_김기한 <jum654@naver.com>
2021-09-06 22:47:01 +0900

    chore: webpack config 및 babel 연동
    - ES Modules를 활용하여 개발하기 위해 webpack 추가
    - 하위 브라우저에도 최신 js 문법을 적용시키기 위해 babel 연동

---
+++ public/javascripts/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document.querySelector('#test').innerText = 'testtest';
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUFBLFFBQVEsQ0FBQ0MsYUFBVCxDQUF1QixPQUF2QixFQUFnQ0MsU0FBaEMsR0FBNEMsVUFBNUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL2phdmFzY3JpcHQtcDItYWlyYm5iLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJyN0ZXN0JykuaW5uZXJUZXh0ID0gJ3Rlc3R0ZXN0JztcclxuIl0sIm5hbWVzIjpbImRvY3VtZW50IiwicXVlcnlTZWxlY3RvciIsImlubmVyVGV4dCJdLCJzb3VyY2VSb290IjoiIn0=
---
+++ src/index.js
@@ -0,0 +1,1 @@
+document.querySelector('#test').innerText = 'testtest';
