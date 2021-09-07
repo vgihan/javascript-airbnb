3c951a50cc547c44e7c6299add4721c459127592
J017_김기한 <jum654@naver.com>
2021-09-07 22:07:39 +0900

    feat: 선택한 항목에 따라 dropdown 변경 구현
    - 체크인, 체크아웃을 누르면 calendar를, 요금을 누르면 가격 범위 설정을,
      인원을 누르면 인원 설정 dropdown이 열리도록 구현.
    - object literal을 이용하여 체크인-체크아웃 간 클릭은 dropdown이
      변경되지 않도록 구현.

--- public/js/search_bar.js
+++ public/js/search_bar.js
@@ -1,14 +1,44 @@
-export function searchItemClickHandler(ev) {
-    showShadowEffect(ev);
-    showCalendarDropdown();
+const searchBar = document.querySelector('.search_bar');
+const searchItems = ['checkin', 'checkout', 'price', 'number'];
+const dropdownOfItem = {
+    'checkin': 'date',
+    'checkout': 'date',
+    'price': 'price',
+    'number': 'number',
 }
 
-function showShadowEffect(ev) {
-    document.querySelectorAll('.search_bar_item').forEach(element => {
+export function searchItemClickHandler(ev) {
+    onCheckedItem(ev);
+    offAllCheckedItem();
+    const targetItem = searchItems.reduce((pre, v) => {
+        if(ev.currentTarget === searchBar.querySelector(`.${v}`)) {
+            pre = v;
+        }
+        return pre;
+    }, '');
+    if(isOpenedDropdown(targetItem)) return;
+    onDropdown(targetItem);
+}
+function isOpenedDropdown(targetItem) {
+    return !document.querySelector(`.search_dropdown.${dropdownOfItem[targetItem]}`).classList.contains('hidden');
+}
+function onCheckedItem(ev) {
+    searchBar.querySelectorAll('.search_bar_item').forEach(element => {
         element.classList.remove('checked_item');
     });
     ev.currentTarget.classList.add('checked_item');
 }
-function showCalendarDropdown() {
-    document.querySelector('.search_dropdown.date').classList.remove('hidden');
+function offAllCheckedItem() {
+    searchBar.querySelectorAll('.search_bar_item').forEach(element => {
+        element.classList.remove('checked_item');
+    });
+}
+function onDropdown(targetClass) {
+    offAllDropdown();
+    document.querySelector(`.search_dropdown.${dropdownOfItem[targetClass]}`).classList.remove('hidden');
+}
+function offAllDropdown() {
+    document.querySelectorAll('.search_dropdown').forEach(element => {
+        element.classList.add('hidden');
+    });
 }
