c281198540c47108011a1e992c7995623a961aab
J017_김기한 <jum654@naver.com>
2021-09-08 18:49:30 +0900

    fix: search view 및 searchItem view 수정
    - searchItemView의 생성자로 event 객체를 전달하지 않는 실수 수정
    - insertAdjacentHTML의 사용으로 submit_btn이 맨처음으로 오게 되는 오류
      수정

--- public/js/search.js
+++ public/js/search.js
@@ -2,18 +2,20 @@ import { SearchItemView } from './search_item.js';
 
 export class SearchView {
     constructor(items, event) {
+        this.event = event;
         this.render();
         this.items = items.reduce((pre, v) => {
-            pre.push(new SearchItemView(v.className, v.title, v.placeholder));
+            pre.push(new SearchItemView(v.className, v.title, v.placeholder, event));
             return pre;
         }, []);
+        const submitBtn = `<div class="search_bar_item submit">
+            <div class="submit_btn"></div>
+        </div>`;
+        this.element = document.querySelector(`.search_bar`);
+        this.element.insertAdjacentHTML('beforeend', submitBtn);
     }
     render() {
-        const html = `<div class="search_bar">
-            <div class="search_bar_item submit">
-                <div class="submit_btn"></div>
-            </div>
-        </div>`;
+        const html = `<div class="search_bar"></div>`;
         document.querySelector('.content_wrap').insertAdjacentHTML('beforeend', html);
     }
 }
