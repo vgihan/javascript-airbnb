4e9fbd0139d969f860b2ace1a57705ddc673f0f2
J017_김기한 <jum654@naver.com>
2021-09-08 20:58:12 +0900

    feat: mainView 구성 및 검색 바 item 수정
    - item click event handler를 bind 하지 않아 this를 인식하지 못하여
      toggleCheckItem 함수 내 this를 참조하는 모든 변수가 undefined로
      출력되는 오류가 발생.
    - bind 함수를 통해 this를 고정시켜줌으로써 해결함.
    - mainView에서 searchItemView의 정보들을 담고 있는 object literal을
      만들어 searchView로 전달함.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,22 @@
+import Event from 'events';
 import addImage from "./add_image.js";
-import {searchItemClickHandler} from "./search_bar.js";
+import { SearchView } from './search.js';
 
-document.querySelectorAll('.search_bar_item').forEach(element => {
-  element.addEventListener('click', searchItemClickHandler);
-});
+class MainView {
+  constructor() {
+    this.event = new Event();
+    this.createSearchView();
+  }
+  createSearchView() {
+    const items = [
+      {className: 'checkin', title: '체크인', placeholder: '날짜 입력'},
+      {className: 'checkout', title: '체크아웃', placeholder: '날짜 입력'},
+      {className: 'price', title: '요금', placeholder: '금액대 설정'},
+      {className: 'number', title: '인원', placeholder: '게스트 추가'},
+    ];
+    this.searchView = new SearchView(items, this.event);
+  }
+}
+
+const mainView = new MainView();
 addImage();
