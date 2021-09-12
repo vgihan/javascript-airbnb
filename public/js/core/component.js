873c4f53f51a94ae5abc7a8ff071e46cc100eaee
J017_김기한 <jum654@naver.com>
2021-09-12 17:46:35 +0900

    refactor: dropdown 컴포넌트 수정
    - dropdown 내부에는 calendar, price, number view가 들어갈 수 있음.
    - dropdown 하위 컴포넌트 세 개를 작성하여 해당 컴포넌트들의 상태를
      이용하여 dropdown 내부에 렌더링할 요소를 선택하도록 구성할 계획이기
      때문에 dropdown class에는 div 태그만을 렌더링함.

--- public/js/core/component.js
+++ public/js/core/component.js
@@ -1,22 +1,31 @@
 export class Component {
     $target;
     $state;
-    constructor($target) {
+    $props;
+    constructor($target, $props) {
         this.$target = $target;
+        this.$props = $props;
         this.setup();
         this.render();
+        this.setEvent();
     }
     setup() {}
     template() {
         return "";
     }
+    mounted() {}
     render() {
         this.$target.innerHTML = this.template();
-        this.setEvent();
+        this.mounted();
     }
     setEvent() {}
     setState(newState) {
         this.$state = newState;
         this.render();
+    }
+    addEvent(eventName, selector, callback) {
+        this.$target
+            .querySelector(selector)
+            .addEventListener(eventName, callback);
     }
 }
