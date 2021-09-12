0186ec8d162b9afbe75822b2096f1fad58213339
J017_김기한 <jum654@naver.com>
2021-09-12 16:39:28 +0900

    refactor: 이벤트 위임을 통한 검색 바 이벤트 핸들링
    - emitter를 이용하여 이벤트를 전달하는 방식에서, 이벤트 위임을 통한
      checked_item 클래스 추가를 진행함.-

--- public/js/components/bar.js
+++ public/js/components/bar.js
@@ -5,22 +5,22 @@ export class Bar extends Component {
         this.$target = document.querySelector("search_box");
         this.$state = {
             checkin: {
-                checkClass: this.$state["checkin"],
+                check: "",
                 title: "체크인",
                 placeholder: "날짜 입력",
             },
             checkout: {
-                checkClass: this.$state["checkout"],
+                check: "",
                 title: "체크아웃",
                 placeholder: "날짜 입력",
             },
             price: {
-                checkClass: this.$state["price"],
+                check: "",
                 title: "요금",
                 placeholder: "금액대 입력",
             },
             number: {
-                checkClass: this.$state["number"],
+                check: "",
                 title: "인원",
                 placeholder: "게스트 추가",
             },
@@ -33,7 +33,7 @@ export class Bar extends Component {
         const items = Object.keys(this.$state).reduce((pre, className) => {
             pre += searchItemTemplate
                 .replace("{{className}}", className)
-                .replace("{{checkClass}}", info[className].checkClass)
+                .replace("{{checkClass}}", info[className].check)
                 .replace("{{title}}", info[className].title)
                 .replace("{{placeholder}}", info[className].placeholder);
             return pre;
@@ -46,6 +46,38 @@ export class Bar extends Component {
         </div>`;
     }
     setEvent() {
-        document.querySelector("search_bar").addEventListener("click");
+        document
+            .querySelector("search_bar")
+            .addEventListener("click", this.changeCheckState);
+    }
+    changeCheckState(ev) {
+        const items = ["checkin", "checkout", "price", "number"];
+        items.forEach((item) => {
+            const isBarItem = ev.target.classList.contains(item);
+            const isChecked = this.$state[item].check === "checked_item";
+            if (isBarItem && !isChecked) {
+                this.setState(this.toggleCheck(this.$state, item));
+            } else if (isBarItem && isChecked) {
+                this.setState(this.removeCheck(this.$state, item));
+            }
+        });
+    }
+    removeCheck(state, target) {
+        return Object.keys(state).reduce((pre, item) => {
+            if (target === item) {
+                pre[item].check = "";
+            }
+            return pre;
+        }, state);
+    }
+    toggleCheck(state, target) {
+        return Object.keys(state).reduce((pre, item) => {
+            if (target === item) {
+                pre[item].check = "checked_item";
+            } else {
+                pre[item].check = "";
+            }
+            return pre;
+        }, state);
     }
 }
