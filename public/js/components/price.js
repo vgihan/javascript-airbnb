05faa5ccff8428e890424ee534fc1d0289d08288
J017_김기한 <jum654@naver.com>
2021-09-13 18:30:55 +0900

    feat: 검색 바 요금, 인원 입력 드롭다운 layout 구성
    - 레인지 슬라이더는 백엔드 구현이 끝난 후 구현하도록 계획함.
    - 요금을 입력할 수 있는 text input box 두 개를 이용해 입력하도록
      구현함.
    - search 컴포넌트에서 state를 저장하여 props로 각 컴포넌트로 상태값을
      전달한다.

--- public/js/components/price.js
+++ public/js/components/price.js
@@ -1,13 +1,13 @@
 import { Component } from "../core/component";
 
 export class Price extends Component {
-    render() {
+    template() {
         return `<div class="price_box">
             <div class="price_title">
                 <p>가격 범위</p>
             </div>
-            <input type="text" id="price_min">
-            <input type="text" id="price_max">
+            <input type="text" id="price_min" placeholder="최소">
+            <input type="text" id="price_max" placeholder="최대">
         </div>`;
     }
 }
