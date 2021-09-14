dc7069484c343a43a0a9f3716d24f8d0df31cd4e
J017_김기한 <jum654@naver.com>
2021-09-14 15:57:27 +0900

    feat: list 컴포넌트 클래스 생성 및 App.js submit 함수 작성
    - 하위 컴포넌트에서 검색 조건 상태를 받을 수 있도록 화살표 함수를
      작성함.

--- public/js/components/app.js
+++ public/js/components/app.js
@@ -3,10 +3,27 @@ import { addImage } from "./add_image";
 import { Component } from "../core/component";
 
 class App extends Component {
+    setup() {
+        this.$state = {
+            page: "",
+        };
+    }
     mounted() {
         const $wrap = document.querySelector(".content_wrap");
+        const { page } = this.$state;
 
-        new Main($wrap, {});
+        new Main($wrap, { submit: this.submit() });
+    }
+    submit() {
+        return ({
+            checkin,
+            checkout,
+            minPrice,
+            maxPrice,
+            numOfAdult,
+            numOfChild,
+            numOfBaby,
+        }) => {};
     }
 }
 
